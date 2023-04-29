862cd4d67737115f2f83d403c5bf160ea162d1a5
Andrew Zhao Luo <andrew.zhao.luo@gmail.com>
2023-04-28 22:33:16 -0700

    initial testbench, initial module

---
+++ base_torch_module.py
@@ -0,0 +1,56 @@
+import torch 
+from torch import nn 
+import numpy as np 
+from typing import Optional
+
+SQRT_2_OVERPI = np.sqrt(2 / np.pi)
+FAST_GELU_INNER_CONST = 0.044715
+
+def gelu_fast(x: torch.Tensor) -> torch.Tensor:
+    return 0.5 * x * (1.0 + torch.tanh(SQRT_2_OVERPI * x * (1.0 + FAST_GELU_INNER_CONST * x * x)))
+
+class BaseTransformerGatedLinearLayer(nn.Module):
+    def __init__(self, dimension_in: int, projection_factor: int = 8, dtype: torch.dtype=torch.half) -> None:
+        super().__init__()
+        
+        if projection_factor % 2 != 0 or projection_factor < 2:
+            raise ValueError("Projection factor must be even and >= 2.")
+        
+        self.dimension_in = dimension_in
+        self.projection_factor = projection_factor
+        self.linear = nn.Linear(self.dimension_in, self.dimension_in * self.projection_factor, bias=True, dtype=dtype)
+        
+    def forward(self, x: torch.Tensor):
+        """Expect x to be shape [batch, dimension_in]."""
+        x = self.linear(x)
+        x1, x2 = x.chunk(2, dim=(x.ndim - 1))
+        return x1 * gelu_fast(x2)
+
+if __name__ == "__main__":
+    """Example of using module."""
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--input-dim", type=int, required=True)
+    parser.add_argument("--batch-size", type=int, required=True)
+    parser.add_argument("--seed", type=Optional[int], default=None)
+    args = parser.parse_args()
+        
+    input_tensor = torch.randn(args.batch_size, args.input_dim, dtype=torch.half)
+    layer = BaseTransformerGatedLinearLayer(args.input_dim)
+    
+    input_tensor = input_tensor.to('cuda')
+    layer = layer.to('cuda')
+    
+    # Forward pass 
+    output_tensor = layer(input_tensor)
+    
+    # Backwards pass
+    loss = torch.sum(torch.abs(output_tensor))
+    loss.backward(retain_graph=True)
+        
+    # Print outputs
+    print("Output: ")
+    print(output_tensor)
+    print()
+    print("Grad: ")
+    print(layer.linear.weight.grad)
---
+++ benchmark.py
@@ -0,0 +1,50 @@
+from base_torch_module import BaseTransformerGatedLinearLayer
+import torch 
+import triton
+from contextlib import ExitStack
+
+QUANTILES_TO_REPORT = [0.5, 0.2, 0.8]
+
+def benchmark_backward(batch_size: int, input_dim: int, projection_factor: int = 8, dtype: torch.dtype=torch.half):
+    torch.cuda.reset_peak_memory_stats(device='cuda')
+    x = torch.randn(batch_size, input_dim, dtype=dtype, device='cuda')
+        
+    module = BaseTransformerGatedLinearLayer(input_dim, projection_factor=projection_factor, dtype=dtype)
+    module.to('cuda')
+        
+    memory_baseline = torch.cuda.max_memory_allocated(device='cuda')    
+    loss = torch.sum(torch.abs(module(x)))
+    ms50, ms20, ms80 = triton.testing.do_bench(lambda: loss.backward(retain_graph=True), percentiles=QUANTILES_TO_REPORT, grad_to_none=[x])
+    peak_memory_backward = torch.cuda.max_memory_allocated(device='cuda')
+    print(f"Backward: {ms50}, {ms20}, {ms80}")
+    print(f"Backward Mem: {peak_memory_backward - memory_baseline}")
+    
+
+def benchmark_forward(batch_size: int, input_dim: int, projection_factor: int = 8, dtype: torch.dtype=torch.half, no_grad: bool = False):
+    torch.cuda.reset_peak_memory_stats(device='cuda')
+    
+    x = torch.randn(batch_size, input_dim, dtype=dtype, device='cuda')
+    module = BaseTransformerGatedLinearLayer(input_dim, projection_factor=projection_factor, dtype=dtype)
+    module.to('cuda')
+    
+    memory_baseline = torch.cuda.max_memory_allocated(device='cuda')
+    
+    with ExitStack() as stack:
+        if no_grad:
+            stack.enter_context(torch.no_grad())
+        ms50, ms20, ms80 = triton.testing.do_bench(lambda: module(x), percentiles=QUANTILES_TO_REPORT)
+    
+    peak_memory_forward = torch.cuda.max_memory_allocated(device='cuda')
+    print(f"Forward: {ms50}, {ms20}, {ms80}")
+    print(f"Forward Mem: {peak_memory_forward - memory_baseline}")
+        
+
+if __name__ == "__main__":
+    """Benchmark implementations.
+    
+    Note before going run the following commands to fix clock speeds (suggest normal baselines):
+        - TODO
+    """
+    benchmark_forward(1024, 4096 * 4) 
+    benchmark_forward(1024, 4096 * 4, no_grad=True) 
+    benchmark_backward(1024, 4096 * 4)
