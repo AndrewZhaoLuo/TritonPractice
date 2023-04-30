0eaf04a4891b8f72ce2d279658362ed036062749
Andrew Zhao Luo <andrew.zhao.luo@gmail.com>
2023-04-29 19:03:17 -0700

    e2e forward kernel

--- triton_kernel.py
+++ triton_kernel.py
@@ -29,6 +29,7 @@ Basic idea backward:
     By wolfram alpha fast_gelu'(x) = 0.5 tanh(a(b * x^3 + x)) 
                                     + a * x (1.5 * b * x^2 + 0.5) * sech^2(a(b x^3 + x))
                                     + 0.5
+    Where a = SQRT_2_OVERPI and b = FAST_GELU_INNER_CONST
                                     
     So dY/dw1 = fast_gelu(x2) * dx1/dw1    
        dY/dw2 = x1 * fast_gelu'(x2) * dx2/dw2 
@@ -38,3 +39,236 @@ Basic idea backward:
     To calculate this efficiently you can just do. We don't need to store any intermediate tensors.
     TODO: deal with transposed weights
 """
+
+import torch 
+import triton 
+import triton.language as tl 
+from torch_module import SQRT_2_OVERPI, FAST_GELU_INNER_CONST, gelu_fast
+import itertools
+from typing import NamedTuple, Optional
+
+class KernelLaunchParameters(NamedTuple):
+    block_size_m: int 
+    block_size_n: int 
+    block_size_k: int 
+    group_size_m: int
+
+@triton.jit 
+def transformer_gated_linear_forward_kernel(
+    in_ptr, 
+    weight_ptr,
+    bias_ptr,
+    out_ptr,
+    # Matrix dimensions
+    M, 
+    two_N,
+    K,
+    stride_in_m, stride_in_k,
+    stride_weight_n, stride_weight_k,
+    stride_bias_n,
+    stride_out_m, stride_out_n,
+    # Meta-parameters
+    ## Shared-Memory Blocking 
+    BLOCK_SIZE_M: tl.constexpr,
+    BLOCK_SIZE_N: tl.constexpr,
+    BLOCK_SIZE_K: tl.constexpr,
+    ## L2 Blocking
+    GROUP_SIZE_M: tl.constexpr,
+):
+    """Calculates the equivalent of the following:
+    
+    T_in     : [M, K]
+    T_weight : [2N, K]
+    T_bias   : [2N]
+    T_out    : [M, N]
+    
+    T_weight1 = T_weight[:N,:]
+    T_bias1 = T_bias[:N]
+    T_weight2 = T_weight[N:,:]
+    T_bias2 = T_bias[N:]
+
+    x1: [M, N] = T_in @ T_weight1.T + T_bias1
+    x2: [M, N] = T_in @ T_weight2.T + T_bias2
+
+    T_out = x1 * fast_gelu(x2)
+    
+    We do this by calculating buf = fast_gelu(x2) in tiles.
+    We then calculate x1's corresponding tiles and then do 
+    x1 * buf with these two pieces in shared memory.
+    """
+    
+    pid = tl.program_id(axis=0)
+    N = two_N // 2
+    
+    # Calculate tiling structures for subproblems calculating x1 and x2
+    m_tiles = tl.cdiv(M, BLOCK_SIZE_M)
+    n_tiles = tl.cdiv(N, BLOCK_SIZE_N)
+        
+    # L2 blocking by group
+    num_pid_in_group = GROUP_SIZE_M * n_tiles
+    group_number = pid // num_pid_in_group 
+    group_start_pid = group_number * GROUP_SIZE_M
+    group_size_m = min(m_tiles - group_start_pid, GROUP_SIZE_M)
+    
+    # Map final output tile indices
+    pid_tile_m = group_start_pid + (pid % group_size_m)
+    pid_tile_n = (pid % num_pid_in_group) // group_size_m
+    
+    # Part 1: fast_gelu(x2) calculation
+    accumulator_x2 = tl.zeros((BLOCK_SIZE_M, BLOCK_SIZE_N), dtype=tl.float32)    
+    weight2_ptr = weight_ptr + N * stride_weight_n
+    bias2_ptr = bias_ptr + N * stride_bias_n
+    accumulator_x2 = calculate_linear_tile(
+        accumulator_x2, 
+        in_ptr, 
+        weight2_ptr, 
+        bias2_ptr,
+        pid_tile_m, 
+        pid_tile_n, 
+        M, N, K, 
+        stride_in_m, stride_in_k, 
+        stride_weight_n, stride_weight_k, 
+        stride_bias_n,
+        BLOCK_SIZE_M, BLOCK_SIZE_N, BLOCK_SIZE_K
+    )
+    accumulator_x2 = fast_gelu_kernel(accumulator_x2)
+        
+    # Part 2: x1 calculation of corresponding tile
+    # TODO: parallelize this across SM + synchronization
+    accumulator_x1 = tl.zeros((BLOCK_SIZE_M, BLOCK_SIZE_N), dtype=tl.float32)    
+    weight1_ptr = weight_ptr
+    bias1_ptr = bias_ptr
+    accumulator_x1 = calculate_linear_tile(
+        accumulator_x1, 
+        in_ptr, 
+        weight1_ptr, 
+        bias1_ptr,
+        pid_tile_m, 
+        pid_tile_n, 
+        M, N, K, 
+        stride_in_m, stride_in_k, 
+        stride_weight_n, stride_weight_k, 
+        stride_bias_n,
+        BLOCK_SIZE_M, BLOCK_SIZE_N, BLOCK_SIZE_K
+    )
+
+    # Part 3: combine the results
+    accumulator_x1 *= accumulator_x2
+
+    # TODO: parametrize casting to dtype
+    out = accumulator_x1.to(tl.float16)
+    offs_out_m = pid_tile_m * BLOCK_SIZE_M + tl.arange(0, BLOCK_SIZE_M)
+    offs_out_n = pid_tile_n * BLOCK_SIZE_N + tl.arange(0, BLOCK_SIZE_N)
+    out_ptrs = out_ptr + stride_out_m * offs_out_m[:, None] + stride_out_n * offs_out_n[None, :]
+    mask = (offs_out_m[:, None] < M) & (offs_out_n[None, :] < N)
+    tl.store(out_ptrs, out, mask=mask)
+
+@triton.jit
+def calculate_linear_tile(
+    accumulator, 
+    in_ptr,
+    weight_ptr,
+    bias_ptr,
+    pid_tile_m, 
+    pid_tile_n, 
+    # Dimensions for iterating
+    M, N, K, 
+    stride_in_m, stride_in_k,
+    stride_weight_n, stride_weight_k,
+    stride_bias_n,
+    # Block size information
+    BLOCK_SIZE_M, BLOCK_SIZE_N, BLOCK_SIZE_K
+):
+    """Calculates the tile at (pid_tile_m, pid_tile_n)"""
+    k_tiles = tl.cdiv(K, BLOCK_SIZE_K)
+
+    offs_in_m = (pid_tile_m * BLOCK_SIZE_M + tl.arange(0, BLOCK_SIZE_M)) % M
+    offs_weight_n = (pid_tile_n * BLOCK_SIZE_N + tl.arange(0, BLOCK_SIZE_N)) % N
+    offs_k = tl.arange(0, BLOCK_SIZE_K)
+    in_load_ptrs = in_ptr + (offs_in_m[:, None] * stride_in_m + offs_k[None, :] * stride_in_k)
+    weight_load_ptrs = weight_ptr + (offs_weight_n[:, None] * stride_weight_n + offs_k[None, :] * stride_weight_k)
+    for k in range(0, k_tiles):
+        T_in = tl.load(in_load_ptrs, mask=offs_k[None, :] < K - k * BLOCK_SIZE_K, other=0.0) 
+        T_weight = tl.load(weight_load_ptrs, mask=offs_k[None, :] < K - k * BLOCK_SIZE_K, other=0.0)        
+        
+        # TODO: think about memory implications of transpose 
+        # Pro: slightly different ordering of loops is faster
+        # Con: using tl.dot probably allows Tensorcore out of box
+        accumulator += tl.dot(T_in, tl.trans(T_weight))
+        in_load_ptrs += BLOCK_SIZE_K * stride_in_k
+        weight_load_ptrs += BLOCK_SIZE_K * stride_weight_k
+        
+    offs_bias = pid_tile_n * BLOCK_SIZE_N + tl.arange(0, BLOCK_SIZE_N)
+    offs_bias = offs_bias[None, :]
+    bias_load_ptrs = bias_ptr + (offs_bias * stride_bias_n)
+    T_bias = tl.load(bias_load_ptrs, mask=offs_bias < (2 * N), other=0.0)     
+    accumulator += T_bias
+    
+    return accumulator
+        
+@triton.jit
+def fast_gelu_kernel(buffer):
+    return 0.5 * buffer * (1.0 + tl.libdevice.tanh(SQRT_2_OVERPI * buffer * (1.0 + FAST_GELU_INNER_CONST * buffer * buffer)))
+
+def transformer_gated_linear_forward(input_tensor: torch.Tensor, weight_tensor: torch.Tensor, bias_tensor: torch.Tensor, kernel_launch_parameters: Optional[KernelLaunchParameters]) -> torch.Tensor:
+    # Check constraints.
+    assert len(bias_tensor.shape) == 1, "Bias should have one dimension"
+    assert bias_tensor.shape[0] == weight_tensor.shape[0], "Incompatible bias dimensions"
+    assert input_tensor.shape[1] == weight_tensor.shape[1], "Incompatible reduction dimensions"
+    assert input_tensor.is_contiguous(), "Matrix A must be contiguous"
+    assert weight_tensor.is_contiguous(), "Matrix B must be contiguous"
+    M, K = input_tensor.shape
+    two_N, K = weight_tensor.shape
+    
+    assert two_N % 2 == 0, "Output dimension must be even."
+    N = two_N // 2
+    
+    # Allocates output.
+    output_tensor = torch.empty((M, N), device=input_tensor.device, dtype=input_tensor.dtype)        
+    grid = lambda META: (
+        triton.cdiv(M, META['BLOCK_SIZE_M']) * triton.cdiv(N, META['BLOCK_SIZE_N']),
+    )
+      
+    # TODO: figure out how autotuning works to make this cleaner
+    transformer_gated_linear_forward_kernel[grid](
+        input_tensor, 
+        weight_tensor, 
+        bias_tensor, 
+        output_tensor,
+        M, two_N, K,
+        input_tensor.stride(0), input_tensor.stride(1),
+        weight_tensor.stride(0), weight_tensor.stride(1),
+        bias_tensor.stride(0), 
+        output_tensor.stride(0), output_tensor.stride(1),
+        kernel_launch_parameters.block_size_m,
+        kernel_launch_parameters.block_size_n,
+        kernel_launch_parameters.block_size_k,
+        kernel_launch_parameters.group_size_m
+    )
+    return output_tensor
+
+if __name__ == "__main__":
+    torch.manual_seed(0)
+
+    M = [312, 512, 1000]
+    two_N = [312, 512]
+    K = [30, 80]
+    
+    kernel_launch_parameters = KernelLaunchParameters(block_size_m=16, block_size_n=64, block_size_k=16, group_size_m=4)
+    for m, two_n, k in itertools.product(M, two_N, K):
+        T_in = torch.randn((m, k), device='cuda', dtype=torch.float16)
+        T_weight = torch.randn((two_n, k), device='cuda', dtype=torch.float16)
+        T_bias = torch.randn((two_n,), device='cuda', dtype=torch.float16)
+        
+        triton_output = transformer_gated_linear_forward(T_in, T_weight, T_bias, kernel_launch_parameters=kernel_launch_parameters)
+        torch_output = T_in @ T_weight.T + T_bias
+        x1, x2 = torch_output.chunk(2, dim=(torch_output.ndim - 1))
+        expected_torch_output = x1 * gelu_fast(x2)
+        
+        print(f"M: {m:<6} 2N: {two_n:<6} K: {k:<6}")
+        # TODO: think about if these tolerances are too high
+        if torch.allclose(triton_output, expected_torch_output, atol=1e-1, rtol=1e-1):
+            print("✅ Triton and Torch match")
+        else:
+            print("❌ Triton and Torch differ")
+        print()
